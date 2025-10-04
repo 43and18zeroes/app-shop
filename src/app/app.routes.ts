dd0a0cb5c1c8f581b1d16c59e88b4f7af19a7517
Christoph <c.wagner.germany@gmail.com>
2025-10-04 15:40:42 +0200

    startpath

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,9 @@
 import { Routes } from '@angular/router';
-import { Apps } from './pages/apps/apps';
-import { Info } from './pages/info/info';
 
 export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'start',
-  },
-  {
-    path: '',
     loadComponent: () => import('./pages/apps/apps').then((m) => m.Apps),
   },
   {
