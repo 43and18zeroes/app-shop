bf6cf789fd1211aab6246d606a9d4d6b218b6674
Christoph <c.wagner.germany@gmail.com>
2025-10-05 16:48:19 +0200

    swiper

--- src/app/pages/apps/apps.ts
+++ src/app/pages/apps/apps.ts
@@ -1,11 +1,64 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
 
 @Component({
   selector: 'app-apps',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './apps.html',
-  styleUrl: './apps.scss'
+  styleUrl: './apps.scss',
 })
 export class Apps {
+  
+  @ViewChild('swiperContainer') swiperContainer!: ElementRef;
+  swiperInstance: Swiper | null = null;
 
+  ngAfterViewInit(): void {
+    if (this.swiperContainer && this.swiperContainer.nativeElement) {
+      this.swiperInstance = new Swiper(this.swiperContainer.nativeElement, {
+        modules: [Navigation],
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        spaceBetween: 28,
+        loop: false,
+        pagination: false,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+          320: {
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+            spaceBetween: 10,
+          },
+          768: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+            spaceBetween: 20,
+          },
+          992: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+            spaceBetween: 20,
+          },
+          1200: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+            spaceBetween: 28,
+          },
+        },
+      });
+    } else {
+      console.error('Swiper container not found!');
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.swiperInstance) {
+      this.swiperInstance.destroy(true, true);
+      this.swiperInstance = null;
+    }
+  }
 }
