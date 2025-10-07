ece508f02dafce58eb555d05db5e44111ba29193
Christoph <c.wagner.germany@gmail.com>
2025-10-07 17:37:11 +0200

    thumbnails styling

--- src/app/pages/apps/apps.ts
+++ src/app/pages/apps/apps.ts
@@ -11,43 +11,73 @@ import 'swiper/swiper-bundle.css';
   styleUrl: './apps.scss',
 })
 export class Apps {
-
   section = [
-  {
-    thumbnailUrl: 'img/thumbnails/thumbnail-rnd-cam.webp',
-    altText: 'Thumbnail 1'
-  },
-  {
-    thumbnailUrl: 'img/thumbnails/thumbnail-rnd-chat.webp',
-    altText: 'Thumbnail 2'
-  },
-  {
-    thumbnailUrl: 'img/thumbnails/thumbnail-rnd-maps.webp',
-    altText: 'Thumbnail 3'
-  },
-  {
-    thumbnailUrl: 'img/thumbnails/thumbnail-rnd-music.webp',
-    altText: 'Thumbnail 4'
-  },
-  {
-    thumbnailUrl: 'img/thumbnails/thumbnail-rnd-music-heart.webp',
-    altText: 'Thumbnail 5'
-  },
-  {
-    thumbnailUrl: 'img/thumbnails/thumbnail-rnd-pad.webp',
-    altText: 'Thumbnail 6'
-  },
-  {
-    thumbnailUrl: 'img/thumbnails/thumbnail-rnd-settings.webp',
-    altText: 'Thumbnail 7'
-  },
-  {
-    thumbnailUrl: 'img/thumbnails/thumbnail-rnd-weather.webp',
-    altText: 'Thumbnail 8'
-  },
-  // Weitere Thumbnails können hier hinzugefügt werden
-];
-  
+    {
+      thumbnailUrl: 'img/thumbnails/thumbnail-rnd-cam.webp',
+      altText: 'Thumbnail 1',
+    },
+    {
+      thumbnailUrl: 'img/thumbnails/thumbnail-rnd-chat.webp',
+      altText: 'Thumbnail 2',
+    },
+    {
+      thumbnailUrl: 'img/thumbnails/thumbnail-rnd-maps.webp',
+      altText: 'Thumbnail 3',
+    },
+    {
+      thumbnailUrl: 'img/thumbnails/thumbnail-rnd-music.webp',
+      altText: 'Thumbnail 4',
+    },
+    {
+      thumbnailUrl: 'img/thumbnails/thumbnail-rnd-music-heart.webp',
+      altText: 'Thumbnail 5',
+    },
+    {
+      thumbnailUrl: 'img/thumbnails/thumbnail-rnd-pad.webp',
+      altText: 'Thumbnail 6',
+    },
+    {
+      thumbnailUrl: 'img/thumbnails/thumbnail-rnd-settings.webp',
+      altText: 'Thumbnail 7',
+    },
+    {
+      thumbnailUrl: 'img/thumbnails/thumbnail-rnd-weather.webp',
+      altText: 'Thumbnail 8',
+    },
+    {
+      thumbnailUrl: 'img/thumbnails/thumbnail-rnd-cam.webp',
+      altText: 'Thumbnail 9',
+    },
+    {
+      thumbnailUrl: 'img/thumbnails/thumbnail-rnd-chat.webp',
+      altText: 'Thumbnail 10',
+    },
+    {
+      thumbnailUrl: 'img/thumbnails/thumbnail-rnd-maps.webp',
+      altText: 'Thumbnail 11',
+    },
+    {
+      thumbnailUrl: 'img/thumbnails/thumbnail-rnd-music.webp',
+      altText: 'Thumbnail 12',
+    },
+    {
+      thumbnailUrl: 'img/thumbnails/thumbnail-rnd-music-heart.webp',
+      altText: 'Thumbnail 13',
+    },
+    {
+      thumbnailUrl: 'img/thumbnails/thumbnail-rnd-pad.webp',
+      altText: 'Thumbnail 14',
+    },
+    {
+      thumbnailUrl: 'img/thumbnails/thumbnail-rnd-settings.webp',
+      altText: 'Thumbnail 15',
+    },
+    {
+      thumbnailUrl: 'img/thumbnails/thumbnail-rnd-weather.webp',
+      altText: 'Thumbnail 16',
+    },
+  ];
+
   @ViewChild('swiperContainer') swiperContainer!: ElementRef;
   swiperInstance: Swiper | null = null;
 
@@ -55,7 +85,7 @@ export class Apps {
     if (this.swiperContainer && this.swiperContainer.nativeElement) {
       this.swiperInstance = new Swiper(this.swiperContainer.nativeElement, {
         modules: [Navigation],
-        slidesPerView: 4,
+        slidesPerView: 8,
         slidesPerGroup: 4,
         spaceBetween: 28,
         loop: false,
@@ -81,7 +111,7 @@ export class Apps {
             spaceBetween: 20,
           },
           1200: {
-            slidesPerView: 4,
+            slidesPerView: 8,
             slidesPerGroup: 4,
             spaceBetween: 28,
           },
