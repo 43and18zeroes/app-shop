2e8eee4b1cbbf11388ce70f701ea1c77874572a6
Christoph <c.wagner.germany@gmail.com>
2025-10-13 18:11:34 +0200

    arrows hidden on mobile

--- src/app/pages/apps/apps.ts
+++ src/app/pages/apps/apps.ts
@@ -42,9 +42,9 @@ export class Apps {
           prevEl: prev as HTMLElement,
         } as NavigationOptions,
         breakpoints: {
-          320: { slidesPerView: 2, slidesPerGroup: 2, spaceBetween: 10 },
-          768: { slidesPerView: 3, slidesPerGroup: 3, spaceBetween: 20 },
-          992: { slidesPerView: 4, slidesPerGroup: 4, spaceBetween: 20 },
+          320: { slidesPerView: 4, slidesPerGroup: 4, spaceBetween: 10 },
+          768: { slidesPerView: 6, slidesPerGroup: 6, spaceBetween: 20 },
+          992: { slidesPerView: 8, slidesPerGroup: 8, spaceBetween: 20 },
           1200: { slidesPerView: 8, slidesPerGroup: 8, spaceBetween: 28 },
         },
       };
