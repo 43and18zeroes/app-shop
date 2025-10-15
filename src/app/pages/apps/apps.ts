697e97c8b5e85d7dee193860044aaf7ec4611a9d
Christoph <c.wagner.germany@gmail.com>
2025-10-15 18:44:39 +0200

    icons

--- src/app/pages/apps/apps.ts
+++ src/app/pages/apps/apps.ts
@@ -42,10 +42,11 @@ export class Apps {
           prevEl: prev as HTMLElement,
         } as NavigationOptions,
         breakpoints: {
-          320: { slidesPerView: 4, slidesPerGroup: 4, spaceBetween: 10 },
-          768: { slidesPerView: 6, slidesPerGroup: 6, spaceBetween: 20 },
-          992: { slidesPerView: 8, slidesPerGroup: 8, spaceBetween: 20 },
-          1200: { slidesPerView: 8, slidesPerGroup: 8, spaceBetween: 28 },
+          320: { slidesPerView: 3, slidesPerGroup: 3, spaceBetween: 10 },
+          600: { slidesPerView: 4, slidesPerGroup: 4, spaceBetween: 20 },
+          // 840: { slidesPerView: 5, slidesPerGroup: 5, spaceBetween: 20 },
+          960: { slidesPerView: 6, slidesPerGroup: 6, spaceBetween: 28 },
+          1440: { slidesPerView: 8, slidesPerGroup: 8, spaceBetween: 28 },
         },
       };
     };
