c7560f88e08f0af366a427cfe7bf9f5f773a419f
Christoph <c.wagner.germany@gmail.com>
2025-10-26 19:14:45 +0100

    BASE_PATH

--- src/app/pages/applications/applications.data.ts
+++ src/app/pages/applications/applications.data.ts
@@ -1,62 +1,54 @@
 export interface SectionItem {
   id: string;
-  path: string;
   thumbnailUrl: string;
   altText: string;
 }
 
+export const BASE_PATH = 'app-details';
+
 export const SECTION: SectionItem[] = [
   {
     id: 'card-grid',
-    path: 'app-details',
     thumbnailUrl: 'img/thumbnails/thumbnail-card-grid-app.webp',
     altText: 'Thumbnail 1',
   },
   {
     id: 'chat',
-    path: 'app-details',
     thumbnailUrl: 'img/thumbnails/thumbnail-rnd-chat.webp',
     altText: 'Thumbnail 2',
   },
   {
     id: 'maps',
-    path: 'app-details',
     thumbnailUrl: 'img/thumbnails/thumbnail-rnd-maps.webp',
     altText: 'Thumbnail 3',
   },
   {
     id: 'music',
-    path: 'app-details',
     thumbnailUrl: 'img/thumbnails/thumbnail-rnd-music.webp',
     altText: 'Thumbnail 4',
   },
   {
     id: 'heart',
-    path: 'app-details',
     thumbnailUrl: 'img/thumbnails/thumbnail-rnd-heart.webp',
     altText: 'Thumbnail 5',
   },
   {
     id: 'pad',
-    path: 'app-details',
     thumbnailUrl: 'img/thumbnails/thumbnail-rnd-pad.webp',
     altText: 'Thumbnail 6',
   },
   {
     id: 'settings',
-    path: 'app-details',
     thumbnailUrl: 'img/thumbnails/thumbnail-rnd-settings.webp',
     altText: 'Thumbnail 7',
   },
   {
     id: 'weather',
-    path: 'app-details',
     thumbnailUrl: 'img/thumbnails/thumbnail-rnd-weather.webp',
     altText: 'Thumbnail 8',
   },
   {
     id: 'cam',
-    path: 'app-details',
     thumbnailUrl: 'img/thumbnails/thumbnail-rnd-cam.webp',
     altText: 'Thumbnail 9',
   },
@@ -64,4 +56,4 @@ export const SECTION: SectionItem[] = [
 
 export const DOUBLED_SECTION: SectionItem[] = [...SECTION, ...SECTION];
 
-export const SECTION_REVERSED: SectionItem[] = [...DOUBLED_SECTION].reverse();
+export const SECTION_REVERSED: SectionItem[] = [...SECTION, ...SECTION].reverse();
