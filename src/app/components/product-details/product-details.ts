3f3642cad175167fb89950a1c776c90034dc8ae4
Christoph <c.wagner.germany@gmail.com>
2025-10-29 17:50:57 +0100

    logoBasePath

--- src/app/components/product-details/product-details.ts
+++ src/app/components/product-details/product-details.ts
@@ -3,14 +3,8 @@ import { ActivatedRoute, Router } from '@angular/router';
 import {
   SECTION,
   SectionItem,
-  BASE_PATH as DEFAULT_LOGO_PATH,
 } from '../../pages/applications/applications.data';
 import { CommonModule } from '@angular/common';
-
-const LOGO_PATH_BY_FEATURE: Record<string, string> = {
-  'app-details': 'img/applications/logos',
-  'game-details': 'img/games/logos',
-};
 
 @Component({
   selector: 'app-product-details',
@@ -25,14 +19,11 @@ export class ProductDetails {
 
   readonly id = signal<string>(this.route.snapshot.paramMap.get('id') ?? '');
 
-  private readonly feature = (() => {
+  readonly logoBasePath = (() => {
     const path = this.router.url.split('?')[0];
     const segments = path.split('/').filter(Boolean);
     return segments[0] ?? '';
   })();
-
-  readonly logoBasePath =
-    LOGO_PATH_BY_FEATURE[this.feature] ?? DEFAULT_LOGO_PATH;
 
   private readonly navState = this.router.getCurrentNavigation()?.extras
     .state as SectionItem | undefined;
