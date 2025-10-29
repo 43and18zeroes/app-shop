afbfe9f99dd072e35b4180d5376fb48d84bf9c60
Christoph <c.wagner.germany@gmail.com>
2025-10-29 18:12:02 +0100

    LogoSwiper

---
+++ src/app/components/logo-swiper/logo-swiper.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-swiper',
+  imports: [],
+  templateUrl: './logo-swiper.html',
+  styleUrl: './logo-swiper.scss'
+})
+export class LogoSwiper {
+
+}
